0306fb0e055db5c7c6c9bb9a4a953e7587678d36
Drizzle03 <tori1652@naver.com>
2023-05-24 07:00:38 +0900

    해상도최최소화및및비동기처리

--- Asyn.py
+++ Asyn.py
@@ -15,8 +15,8 @@ output_details = interpreter.get_output_details()
 
 # 웹캠 load
 camera = cv2.VideoCapture(0)
-camera.set(3, 640)  # width
-camera.set(4, 480)  # height
+camera.set(3, 320)  # width - lower resolution
+camera.set(4, 240)  # height - lower resolution
 
 class ImageGetter(threading.Thread):
     def __init__(self):
@@ -37,8 +37,37 @@ class ImageGetter(threading.Thread):
             self.image = frame
             time.sleep(0.01)  # sleep for a while to reduce CPU usage
 
+class ModelInferencer(threading.Thread):
+    def __init__(self):
+        super(ModelInferencer, self).__init__()
+        self.prediction = None
+        self.image = None
+        self.new_prediction = False
+
+    def set_image(self, image):
+        self.image = image
+
+    def get_prediction(self):
+        self.new_prediction = False
+        return self.prediction
+
+    def run(self):
+        global interpreter, input_details, output_details
+        while True:
+            if self.image is not None:
+                interpreter.set_tensor(input_details[0]['index'], self.image)
+                interpreter.invoke()
+                self.prediction = interpreter.get_tensor(output_details[0]['index'])
+                self.new_prediction = True
+                self.image = None
+            time.sleep(0.01)  # sleep for a while to reduce CPU usage
+
 image_getter = ImageGetter()
 image_getter.start()
+
+model_inferencer = ModelInferencer()
+model_inferencer.start()
+
 
 class Circle:
     def __init__(self, x, y):
@@ -47,7 +76,8 @@ class Circle:
         self.targetX = self.x
         self.targetY = self.y
         self.easing = 0.05
-        self.size = random.uniform(20, 80)  # 동그라미의 크기 랜덤 설정
+        self.size = random.uniform(20, 80)  
+        # 동그라미의 크기 랜덤 설정
 
     def move(self):
         dx = self.targetX - self.x
@@ -79,29 +109,29 @@ def draw():
         circle.display()
 
     image = image_getter.get_image()  # 웹캠 이미지 가져오기
-    interpreter.set_tensor(input_details[0]['index'], image)
-    # 모델 실행
-    interpreter.invoke()
-    # 예측 결과 도출
-    prediction = interpreter.get_tensor(output_details[0]['index'])
+    if image is not None:
+        model_inferencer.set_image(image)
 
+    if model_inferencer.new_prediction:
+        prediction = model_inferencer.get_prediction()
 
-    # 주먹 - 모임
-    if np.argmax(prediction) == 0: 
-        if status == 1:
-            status = 0
-            targetX = width / 2
-            targetY = height / 2
-            for circle in circles:
-                circle.targetX = targetX
-                circle.targetY = targetY
+        # 주먹 - 모임
+        if np.argmax(prediction) == 0: 
+            if status == 1:
+                status = 0
+                targetX = width / 2
+                targetY = height / 2
+                for circle in circles:
+                    circle.targetX = targetX
+                    circle.targetY = targetY
 
-    # 보자기 - 퍼짐
-    elif np.argmax(prediction) == 1:
-        if status == 0:
-            status = 1
-            for circle in circles:
-                circle.targetX = random.uniform(0, width)
-                circle.targetY = random.uniform(0, height)
+        # 보자기 - 퍼짐
+        elif np.argmax(prediction) == 1:
+            if status == 0:
+                status = 1
+                for circle in circles:
+                    circle.targetX = random.uniform(0, width)
+                    circle.targetY = random.uniform(0, height)
 
 run()
+
