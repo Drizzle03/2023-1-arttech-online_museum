bacaa421310f669fceee8096a8358943bc8a8aa4
drizzle03 <tori1652@naver.com>
2023-06-08 20:14:18 +0900

    source 저장 방식 구조 개선 및 light, smile 기능 추가

--- 02_Merge_functions_task/Merge_functions_task.py
+++ 02_Merge_functions_task/Merge_functions_task.py
@@ -1,7 +1,11 @@
 from p5 import *
+from random import choice, randint
 
 button_images = []  
 active_buttons = []  
+
+smile_img = []
+light_img = []
 
 num = 60
 
@@ -9,49 +13,14 @@ save_deactive_images = ''
 save_button = 'deactive'
 frame = True
 
-# 색상 리스트
-color_list = ["#F2EEE5", "#E5C1C5", "#C3E2DD", "#6ECEDA"]
-color_index = 0
-
-class Spin:
-    def __init__(self, xpos, ypos, s):
-        self.x = xpos
-        self.y = ypos
-        self.speed = s
-        self.angle = 0.0
-
-    def update(self):
-        self.angle += self.speed
-
-
-class SpinSpots(Spin):
-    def __init__(self, x, y, s, d):
-        super().__init__(x, y, s)
-        self.dim = d
-
-    def display(self):
-        no_stroke()
-        push_matrix()
-        translate(mouse_x, mouse_y)  # 변경된 부분
-        self.angle += self.speed
-        rotate(self.angle)
-        ellipse(-self.dim/2, 0, self.dim, self.dim)
-        ellipse(self.dim/2, 0, self.dim, self.dim)
-        pop_matrix()
-
-
-spots = None
-arm = None
 
 def setup():
-    global spots, arm
     size(1280, 720)  
     no_stroke()
     load_button_images()
-    spots = SpinSpots(width/2, height/2, -0.02, 90.0)
 
 def draw():
-    global save_button, frame, spots, arm, color_list, color_index
+    global save_button, frame
     if frame:
         background(255)
         frame = False
@@ -61,33 +30,22 @@ def draw():
 
     # btn1가 활성화되었을 경우
     if 0 in active_buttons:
-        fill(color_list[color_index])
-        spots.update()
-        spots.display()
+        light_choice_img = choice(light_img)
+        image(light_choice_img, randint(0, 1280), randint(0, 600))  # width와 height를 랜덤 크기로 설정
 
-    # btn2가 활성화되었을 경우
-    if 1 in active_buttons: 
-        which = frame_count % num
-        mousey = min(600, mouse_y)  # 마우스의 y 좌표가 613 이상이면 613으로 고정
-
-        fill(color_list[color_index])
-        ellipse(mouse_x, mousey, 30, 30)
-            
-    # 색상 인덱스 갱신
-    color_index = (color_index + 1) % len(color_list)
-
-
-
+    #지우개 버튼
     if 5 in active_buttons: 
         frame = True
+        active_buttons.remove(5)
 
     draw_button()
+
 
 def draw_button():
     global save_button
     for i in range(len(button_images)):
         if i in active_buttons:
-            active_image = load_image(f"source/btn{i+1}-active.png")
+            active_image = load_image(f"source/btn/btn{i+1}-active.png")
             image(active_image, 355+(i*99), 627, 75, 75)  
         else:
             image(button_images[i], 355+(i*99), 627, 75, 75)  
@@ -95,16 +53,36 @@ def draw_button():
     if save_button == 'deactive':
         image(save_deactive_images, 1175, 628, 75, 75)
     else:
-        save_active_image = load_image("source/save-active.png")
+        save_active_image = load_image("source/btn/save-active.png")
         image(save_active_image, 1175, 628, 75, 75)
 
 def load_button_images():
     global save_deactive_images
-    save_deactive_images = load_image('source/save-deactive.png')
+    save_deactive_images = load_image('source/btn/save-deactive.png')
+    
+
+    #btn1 - red image
+    for i in range(4):
+        light_img.append(load_image(f'source/btn1_light/light-red-{100 + (i*50)}.png'))
+
+    #btn1 - yellow image
+    for i in range(4):
+        light_img.append(load_image(f'source/btn1_light/light-yellow-{100 + (i*50)}.png'))
+
+    #btn1 - blue image
+    for i in range(4):
+        light_img.append(load_image(f'source/btn1_light/light-blue-{100 + (i*50)}.png'))
+
+    #btn2 - smile image
+    for i in range(4):
+        smile_img.append(load_image(f'source/btn2_smile/smile{50*(i+1)}.png'))
 
     for i in range(6):
-        button_image = load_image(f"source/btn{i+1}-deactive.png")
+        button_image = load_image(f"source/btn/btn{i+1}-deactive.png")
         button_images.append(button_image)
+
+
+    
 
 def mouse_pressed():
     global save_button
@@ -129,5 +107,11 @@ def mouse_pressed():
             save_button = 'active'
         else:
             save_button = 'deactive'
+
+    if 1 in active_buttons: 
+        mousey = min(600, mouse_y)  # 마우스의 y 좌표가 613 이상이면 613으로 고정
+
+        choice_smile_img = choice(smile_img)  # 50에서 200 사이의 랜덤 크기 스마일 이미지 선택 
+        image(choice_smile_img, mouse_x, mousey)  # width와 height를 랜덤 크기로 설정
 
 run()
